91d32537a7be1d69e9176e85376464b43c81597e
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-06-02 17:27:45 -0600

    feature: funcion hashpassword en userdto

--- src/domain/dto/userDTO.go
+++ src/domain/dto/userDTO.go
@@ -1,8 +1,15 @@
 package dto
 
+import "golang.org/x/crypto/bcrypt"
+
 type UserDTO struct {
 	Name     string `json:"name" validate:"required,alphaspace,min=5,max=50"`
 	UserName string `json:"username" validate:"required,alphanum,min=5,max=30"`
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,password"`
+}
+
+func (dto UserDTO) HashPassword() string {
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(dto.Password), bcrypt.DefaultCost)
+	return string(hashed)
 }
