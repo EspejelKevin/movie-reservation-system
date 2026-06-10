351214b7595e3095299c187ce4ca5d3413c48181
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-06-09 22:50:06 -0600

    feature: desarrollo del caso de uso savemovie y getmovies

---
+++ src/application/usecases/showtime/get_showtimes.go
@@ -0,0 +1,53 @@
+package showtime
+
+import (
+	"movie-reservation-system/src/domain/dto"
+	"movie-reservation-system/src/domain/entities"
+	"movie-reservation-system/src/domain/interfaces"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GetShowTImesUseCase struct {
+	repository interfaces.ShowTimeRepository
+}
+
+func NewGetShowTimesUseCase(repository interfaces.ShowTimeRepository) *GetShowTImesUseCase {
+	return &GetShowTImesUseCase{repository}
+}
+
+func (usecase *GetShowTImesUseCase) Execute(ctx *gin.Context) {
+	shows, err := usecase.repository.GetShows(ctx.Request.Context())
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"shows": usecase.mapShows(shows)})
+}
+
+func (usecase *GetShowTImesUseCase) mapShows(shows []entities.ShowTime) []dto.ShowResponse {
+	showResponse := make([]dto.ShowResponse, 0, len(shows))
+
+	for _, show := range shows {
+		showResponse = append(showResponse, dto.ShowResponse{
+			ID:                     show.ID,
+			Folio:                  show.Folio,
+			StartDate:              show.StartDate.Format("2006-01-02 15:04:05"),
+			EndDate:                show.EndDate.Format("2006-01-02 15:04:05"),
+			AvailableQuantitySeats: show.AvailableQuantitySeats,
+			UnavailableSeats:       show.UnavailableSeats,
+			Movie: dto.MovieResponse{
+				ID:          show.Movie.ID,
+				Title:       show.Movie.Title,
+				Description: show.Movie.Description,
+				Image:       show.Movie.Image.String,
+				Genre:       show.Movie.Genre.Name,
+			},
+		})
+	}
+
+	return showResponse
+}
---
+++ src/application/usecases/showtime/save_showtime.go
@@ -0,0 +1,70 @@
+package showtime
+
+import (
+	"movie-reservation-system/src/domain/dto"
+	"movie-reservation-system/src/domain/entities"
+	"movie-reservation-system/src/domain/interfaces"
+	"net/http"
+	"reflect"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
+type SaveShowTimeUseCase struct {
+	repository interfaces.ShowTimeRepository
+	validate   *validator.Validate
+}
+
+func NewSaveShowTimeUseCase(repository interfaces.ShowTimeRepository,
+	validate *validator.Validate) *SaveShowTimeUseCase {
+	return &SaveShowTimeUseCase{repository, validate}
+}
+
+func (usecase *SaveShowTimeUseCase) Execute(ctx *gin.Context) {
+	var showDTO dto.ShowDTO
+
+	if err := ctx.ShouldBindJSON(&showDTO); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := usecase.validate.Struct(showDTO); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	showDB, err := usecase.repository.GetShowByFolio(ctx.Request.Context(), showDTO.Folio)
+
+	if err != gorm.ErrRecordNotFound && err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !reflect.DeepEqual(showDB, entities.ShowTime{}) {
+		ctx.JSON(http.StatusConflict, gin.H{"error": "show ya existente"})
+		return
+	}
+
+	newShow := entities.ShowTime{
+		Folio:                  showDTO.Folio,
+		StartDate:              usecase.toTime(showDTO.StartDate),
+		EndDate:                usecase.toTime(showDTO.EndDate),
+		AvailableQuantitySeats: showDTO.AvailableQuantitySeats,
+		MovieID:                showDTO.MovieID,
+	}
+
+	if err := usecase.repository.Save(ctx.Request.Context(), newShow); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "registro exitoso"})
+}
+
+func (usecase *SaveShowTimeUseCase) toTime(date string) time.Time {
+	time, _ := time.Parse("2006-01-02 15:04:05", date)
+	return time
+}
