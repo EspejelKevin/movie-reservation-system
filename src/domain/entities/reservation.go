12aed11753b69a1a0a99e907813e6d179a41f2bd
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-06-12 11:13:47 -0600

    feature: default value RESERVED para el campo status

--- src/domain/entities/reservation.go
+++ src/domain/entities/reservation.go
@@ -3,7 +3,7 @@ package entities
 type Reservation struct {
 	ID         uint   `gorm:"primaryKey;autoIncrement"`
 	Folio      string `gorm:"unique,not null"`
-	Status     string `gorm:"not null"`
+	Status     string `gorm:"default:RESERVED"`
 	ShowTimeID uint
 	ShowTime   ShowTime `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	UserID     uint
