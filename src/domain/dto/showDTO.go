6ff5770235778e4e55837dc16634c979b480957e
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-06-12 18:09:29 -0600

    feature: renombramiento del campo quantity_seats

--- src/domain/dto/showDTO.go
+++ src/domain/dto/showDTO.go
@@ -4,7 +4,7 @@ type ShowDTO struct {
 	Folio         string `json:"folio" validate:"required,alphanum,min=8,max=12"`
 	StartDate     string `json:"start_date" validate:"required,datetime=2006-01-02 15:04:05"`
 	EndDate       string `json:"end_date" validate:"required,datetime=2006-01-02 15:04:05,after_start"`
-	QuantitySeats int    `json:"available_quantity_seats" validate:"required,gte=1"`
+	QuantitySeats int    `json:"quantity_seats" validate:"required,gte=1"`
 	MovieID       uint   `json:"movie_id" validate:"required,gte=1"`
 }
 
